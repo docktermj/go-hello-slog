786fb0907b873e6251adb29c2a8387e0b9d766ba
docktermj <michael@dockter.com>
2023-04-05 12:09:17 -0400

    Log level testing

--- main.go
+++ main.go
@@ -3,21 +3,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"golang.org/x/exp/slog"
 )
 
 func main() {
+
+	ctx := context.TODO()
 	logger := slog.New(slog.NewJSONHandler(os.Stderr))
 
-	aSenzingMessage := `
+	// Test JSON messages
+
+	aJsonMessage := `
     {
         "date": "2023-03-23",
         "time": "18:53:15.623837476",
         "level": "ERROR",
-        "id": "senzing-60044001",
+        "id": "error-60044001",
         "text": "Call to G2_addRecord(CUSTOMERS, 1002, {\"DATA_SOURCE\": \"BOB\", \"RECORD_ID\": \"1002\", \"RECORD_TYPE\": \"PERSON\", \"PRIMARY_NAME_LAST\": \"Smith\", \"PRIMARY_NAME_FIRST\": \"Bob\", \"DATE_OF_BIRTH\": \"11/12/1978\", \"ADDR_TYPE\": \"HOME\", \"ADDR_LINE1\": \"1515 Adela Lane\", \"ADDR_CITY\": \"Las Vegas\", \"ADDR_STATE\": \"NV\", \"ADDR_POSTAL_CODE\": \"89111\", \"PHONE_TYPE\": \"MOBILE\", \"PHONE_NUMBER\": \"702-919-1300\", \"DATE\": \"3/10/17\", \"STATUS\": \"Inactive\", \"AMOUNT\": \"200\"}, G2Engine_test) failed. Return code: -2",
         "duration": 505312,
         "location": "In AddRecord() at g2engineserver.go:66",
@@ -53,10 +59,24 @@ func main() {
     `
 
 	jsonMap := make(map[string]interface{})
-	err := json.Unmarshal([]byte(aSenzingMessage), &jsonMap)
+	err := json.Unmarshal([]byte(aJsonMessage), &jsonMap)
 	if err != nil {
 		panic(err)
 	}
 
-	logger.Info("hello", "SenzingMessage", jsonMap)
+	logger.Info("hello", "PriorMessage", jsonMap)
+
+	// Log levels and guards
+
+	fmt.Printf("\nLog Levels: Debug: %d; Info: %d; Warn: %d; Error: %d\n\n", slog.LevelDebug, slog.LevelInfo, slog.LevelWarn, slog.LevelError)
+
+	fmt.Printf("Log Level    -5: %v\n", logger.Enabled(ctx, -5))
+	fmt.Printf("Log Level Debug: %v\n", logger.Enabled(ctx, slog.LevelDebug))
+	fmt.Printf("Log Level    -1: %v\n", logger.Enabled(ctx, -1))
+	fmt.Printf("Log Level  Info: %v\n", logger.Enabled(ctx, slog.LevelInfo))
+	fmt.Printf("Log Level  Warn: %v\n", logger.Enabled(ctx, slog.LevelWarn))
+	fmt.Printf("Log Level Error: %v\n", logger.Enabled(ctx, slog.LevelError))
+	fmt.Printf("Log Level     5: %v\n", logger.Enabled(ctx, 5))
+	fmt.Printf("Log Level     9: %v\n", logger.Enabled(ctx, 9))
+
 }
